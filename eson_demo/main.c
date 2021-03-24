e042f4c85059a76ea3ed1ef354b14b844b22cb40
Eric Meehan <eric.meehan@homodeus.us>
2021-03-24 12:26:54 -0400

    Appended contents of an image file to an eson document

--- eson_demo/main.c
+++ eson_demo/main.c
@@ -8,9 +8,24 @@
 #include "../../libeom/libeom.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+void write_image_into_eson()
+{
+    // Originally main, was used to create example_eson.block
+    FILE *example_eson = fopen("/Users/eric/Documents/sketchbook/example_eson.block", "ab");
+    FILE *image = fopen("/Users/eric/Documents/sketchbook/eric_original.jpg", "rb");
+    
+    // Get the size of the original
+    fseek(image, 0, SEEK_END);
+    long fsize = ftell(image);
+    fseek(image, 0, SEEK_SET);
+    
+    void *data = malloc(fsize + 1);
+    fread(data, fsize, fsize, image);
+    ((char *)data)[fsize] = '}';
+    fclose(image);
+    
+    fwrite(data, fsize + 1, fsize + 1, example_eson);
+    fclose(example_eson);
 }
