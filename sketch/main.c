ec911d5e2c35e0e29fc5c5247bdebfd5674f631d
Eric Meehan <eric.meehan@homodeus.us>
2021-03-24 23:32:56 -0400

    Successfully copied an image file where the everything after a given point was that file's data.

--- sketch/main.c
+++ sketch/main.c
@@ -6,10 +6,53 @@
 //
 
 #include "../../libeom/libeom.h"
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+void write_image_into_eson()
+{
+    // Originally main, was used to create example_eson.block
+    FILE *eson = fopen("/Users/eric/Documents/sketchbook/example.eson", "ab");
+    FILE *eric_original = fopen("/Users/eric/Documents/sketchbook/eric_original.jpg", "rb");
+    
+    // Get the size of the original
+    fseek(eric_original, 0, SEEK_END);
+    long file_size = ftell(eric_original);
+    fseek(eric_original, 0, SEEK_SET);
+    
+    void *file_data = malloc(file_size);
+    fread(file_data, file_size, file_size, eric_original);
+    fclose(eric_original);
+    //((char *)data)[fsize] = '}';
+    
+    fwrite(file_data, file_size, file_size, eson);
+    
+    fclose(eson);
+}
+
+int main()
+{
+    write_image_into_eson();
+    // Now read the contents from eson and write them to a new image file
+    FILE *eson = fopen("/Users/eric/Documents/sketchbook/example.eson", "r");
+    fseek(eson, 0, SEEK_END);
+    long fsize = ftell(eson);
+    fseek(eson, 0, SEEK_SET);
+    
+    void *data = malloc(fsize);
+    fread(data, fsize, fsize, eson);
+    
+    // 274 characters before file
+    
+    void *image = malloc(fsize - 275);
+    for (int i = 275; i < fsize-1; i++)
+    {
+        ((char *)image)[i - 275] = ((char *)data)[i];
+    }
+    
+    FILE *output = fopen("/Users/eric/Desktop/output.jpg", "w");
+    fwrite(image, fsize-275, fsize-275, output);
+    fclose(output);
+    
 }
