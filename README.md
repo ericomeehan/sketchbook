cf7d4185e9fe15ed22d10e227e7d3174146deb3b
Eric Meehan <eric.meehan@homodeus.us>
2021-03-26 18:57:40 -0400

    Demo of copying, modifying, and moving a file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Generic C Project
+# File I/O
+
+Demonstrating reading and writing files as void pointers.  Doing so in this way allows the manipulation of raw bytes.
--- sketch/main.c
+++ sketch/main.c
@@ -7,9 +7,35 @@
 
 #include "../../libeom/libeom.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    FILE *a = fopen("/Users/eric/Desktop/image.jpg", "r");
+    long size = get_file_size(a);
+    
+    char *header = "{data:";
+    void *image = malloc(size + strlen(header));
+    memcpy(image, header, strlen(header));
+    fread(image + strlen(header), size, size, a);
+    fclose(a);
+    
+    FILE *b = fopen("/Users/eric/Desktop/copy.jpg", "w");
+    fwrite(image, size, size, b);
+    fclose(b);
+    
+    b = fopen("/Users/eric/Desktop/copy.jpg", "r");
+    FILE *c = fopen("/Users/eric/Desktop/second_copy.jpg", "w");
+    long b_size = get_file_size(b);
+    
+    void *second_copy = malloc(b_size);
+    fread(second_copy, b_size, b_size, b);
+    fclose(b);
+    
+    void *final_image = malloc(b_size - 6);
+    memcpy(final_image, second_copy + 6, b_size - 6);
+    fwrite(final_image, b_size - 6, b_size - 6, c);
+    fclose(c);
+    
 }
