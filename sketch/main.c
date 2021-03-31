7d102c65aad9f1509f2914a0f35d752ca2a1d826
Eric Meehan <eric.meehan@homodeus.us>
2021-03-31 14:09:13 -0400

    Demonstrated printing UTC time.

--- sketch/main.c
+++ sketch/main.c
@@ -7,9 +7,17 @@
 
 #include "../../libeom/libeom.h"
 #include <stdio.h>
+#include <stdio.h>
+#include <time.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main ()
+{
+  time_t rawtime;
+  struct tm * timeinfo;
+
+  time ( &rawtime );
+  timeinfo = gmtime ( &rawtime );
+  printf ( "%d-%.2d-%.2d %.2d:%.2d:%.2d\n", timeinfo->tm_year + 1900, timeinfo->tm_mon + 1, timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+  
+  return 0;
 }
