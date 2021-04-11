f935433c7bb074c4c4479db0f693ee7b65ff7025
Eric Meehan <eric.meehan@homodeus.us>
2021-04-10 20:57:17 -0400

    Experimenting with command line arguments.

--- sketch/main.c
+++ sketch/main.c
@@ -8,8 +8,17 @@
 #include "../../libeom/libeom.h"
 #include <stdio.h>
 
+enum
+{
+    a,
+};
+
+// argv[0] = program name
+// argv[1] = first arg
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
 }
