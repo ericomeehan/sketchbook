467a11750e491a9242bc58a925758cf95ff58119
Eric Meehan <eric.meehan@homodeus.us>
2021-04-13 10:01:29 -0400

    Terminal 'to' application can tell between stdin being a pipe or interactive.

--- sketch/pipe_from.c
+++ sketch/pipe_from.c
@@ -7,8 +7,14 @@
 
 #include "../../libeom/libeom.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, const char * argv[])
 {
-    
+    printf("%d\n", fileno(stdin));
+    FILE *pipe = popen("/Users/eric/Documents/sketchbook/to", "w");
+    char *message = "hello world";
+    fwrite(message, 12, 1, pipe);
+    pclose(pipe);
+    return 0;
 }
--- sketch/pipe_to.c
+++ sketch/pipe_to.c
@@ -7,8 +7,26 @@
 
 #include "../../libeom/libeom.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, const char *argv[])
 {
+    void *data = alloca(256);
+    memset(data, 0, 256);
+    fread(data, 256, 1, stdin);
     
+    if (isatty(fileno(stdin)))
+    {
+        printf("Terminal\n");
+    }
+    else
+    {
+        printf("Pipe\n");
+    }
+    printf("%s\n", data);
+    
+    return 0;
 }
